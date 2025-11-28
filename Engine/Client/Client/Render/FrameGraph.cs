52173e77a2a247791f32f84293c4ae099f0d5508
GroceyLot <waltermorton@icloud.com>
2025-11-28 16:12:29 +0000

    asd

--- Engine/Client/Client/Render/FrameGraph.cs
+++ Engine/Client/Client/Render/FrameGraph.cs
@@ -1,32 +1,140 @@
 global using Patchwork.Client.Render;
+using OpenTK.Graphics.OpenGL4;
 namespace Patchwork.Client.Render;
 
 public static class FrameGraph
 {
-    public struct GPUTexture
+    public class GPUTexture : IDisposable
     {
-        public GPUTexture(string path)
+        public const uint Magic = 0x694267FF;
+        private readonly int Handle;
+        public readonly long Bindless;
+        public readonly uint Width;
+        public readonly uint Height;
+        public readonly TextureFormat Format;
+        public enum TextureFormat : byte
         {
-
+            Albedo,
+            Normal,
+            MetallicRoughnessAO,
+            Emissive,
         }
 
-    }
-    public struct GPUShader
-    {
-        public GPUShader(string path)
+        public GPUTexture(string path, TextureFormat? format = null)
         {
+            FileStream data = DriveMounts.FileStream(path);
+            using BinaryReader reader = new(data);
 
+            uint magic = reader.ReadUInt32();
+            if (magic != Magic)
+            {
+                throw new Exception("Invalid texture file");
+            }
+
+            Width = reader.ReadUInt32();
+            Height = reader.ReadUInt32();
+            Format = (TextureFormat)reader.ReadByte();
+            if (format != null && format != Format)
+            {
+                throw new InvalidDataException($"Texture file has format {Format} but was expected to be {format}");
+            }
+            bool repeat = reader.ReadBoolean();
+            bool linear = reader.ReadBoolean();
+
+            long remaining = data.Length - data.Position;
+            if (remaining <= 0)
+            {
+                throw new InvalidDataException("Texture file has no pixel data");
+            }
+
+            byte[] pixelData = reader.ReadBytes((int)remaining);
+            if (pixelData.Length == 0)
+            {
+                throw new InvalidDataException("Texture file has no pixel data");
+            }
+
+            Handle = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+
+            GetTextureFormatGL(
+                Format,
+                out PixelInternalFormat internalFormat,
+                out PixelFormat pixelFormat,
+                out PixelType pixelType
+            );
+
+            GL.TexImage2D(
+                TextureTarget.Texture2D,
+                0,
+                internalFormat,
+                (int)Width,
+                (int)Height,
+                0,
+                pixelFormat,
+                pixelType,
+                pixelData
+            );
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, linear ? (int)TextureMinFilter.LinearMipmapLinear : (int)TextureMinFilter.NearestMipmapNearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, linear ? (int)TextureMagFilter.Linear : (int)TextureMagFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, repeat ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, repeat ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
+            Bindless = GL.Arb.GetTextureHandle(Handle);
+            GL.Arb.MakeTextureHandleResident(Bindless);
+        }
+
+        private static void GetTextureFormatGL(
+            TextureFormat format,
+            out PixelInternalFormat internalFormat,
+            out PixelFormat pixelFormat,
+            out PixelType pixelType
+        )
+        {
+            switch (format)
+            {
+                case TextureFormat.Albedo:
+                    internalFormat = PixelInternalFormat.Rgba8;
+                    pixelFormat = PixelFormat.Rgba;
+                    pixelType = PixelType.UnsignedByte;
+                    break;
+                case TextureFormat.Normal:
+                    internalFormat = PixelInternalFormat.Rgb10A2;
+                    pixelFormat = PixelFormat.Rgba;
+                    pixelType = PixelType.UnsignedByte;
+                    break;
+                case TextureFormat.MetallicRoughnessAO:
+                    internalFormat = PixelInternalFormat.Rgb8;
+                    pixelFormat = PixelFormat.Rgb;
+                    pixelType = PixelType.UnsignedByte;
+                    break;
+                case TextureFormat.Emissive:
+                    internalFormat = PixelInternalFormat.Rgb16f;
+                    pixelFormat = PixelFormat.Rgb;
+                    pixelType = PixelType.HalfFloat;
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Texture format {format} is not mapped yet, or requires compression support");
+            }
+        }
+        public void Dispose()
+        {
+            GL.Arb.MakeTextureHandleNonResident(Bindless);
+            GL.DeleteTexture(Handle);
+        }
+        ~GPUTexture()
+        {
+            Dispose();
         }
     }
     public struct GPUMesh
     {
         public GPUMesh(string path)
         {
-            
+
         }
     }
     public static Dictionary<string, GPUTexture> Textures = new();
-    public static Dictionary<string, GPUShader> Shaders = new();
     public static Dictionary<string, GPUMesh> Meshes = new();
     public static bool Build()
     {
@@ -38,23 +146,43 @@ public static class FrameGraph
                 model = model_;
             }
             else continue;
-            if (!Shaders.ContainsKey(model.ShaderPath))
-            {
-                Shaders[model.ShaderPath] = new GPUShader(model.ShaderPath);
-                return false;
-            }
             if (!Meshes.ContainsKey(model.DataPath))
             {
                 Meshes[model.DataPath] = new GPUMesh(model.DataPath);
                 return false;
             }
-            foreach (ITexture texture in model.Textures)
-                if (texture is PathTexture pathTexture)
+            {
+                if (model.Albedo is PathTexture pathTexture)
                     if (!Textures.ContainsKey(pathTexture.Path))
                     {
                         Textures[pathTexture.Path] = new GPUTexture(pathTexture.Path);
                         return false;
                     }
+            }
+            {
+                if (model.Normal is PathTexture pathTexture)
+                    if (!Textures.ContainsKey(pathTexture.Path))
+                    {
+                        Textures[pathTexture.Path] = new GPUTexture(pathTexture.Path);
+                        return false;
+                    }
+            }
+            {
+                if (model.MetallicRoughnessAO is PathTexture pathTexture)
+                    if (!Textures.ContainsKey(pathTexture.Path))
+                    {
+                        Textures[pathTexture.Path] = new GPUTexture(pathTexture.Path);
+                        return false;
+                    }
+            }
+            {
+                if (model.Emissive is PathTexture pathTexture)
+                    if (!Textures.ContainsKey(pathTexture.Path))
+                    {
+                        Textures[pathTexture.Path] = new GPUTexture(pathTexture.Path);
+                        return false;
+                    }
+            }
         }
         return true;
     }
---
+++ Engine/Common/Serialization/Serialization.cs
@@ -0,0 +1,336 @@
+global using Patchwork.Serialization;
+using System.Reflection;
+namespace Patchwork.Serialization;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public sealed class SerializedMemberAttribute(bool save = true) : Attribute
+{
+    public bool Save { get; init; } = save;
+}
+
+public struct SerializedMember
+{
+    public MemberInfo Member;
+    public Type Type;
+    public bool Save;
+}
+public static class SerializationRegistry
+{
+    private static Dictionary<Type, Dictionary<string, SerializedMember>> Members = new();
+    private static void Register(Type type)
+    {
+        Dictionary<string, SerializedMember> members = new();
+        foreach (MemberInfo member in type.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+        {
+            SerializedMemberAttribute? attribute = member.GetCustomAttribute<SerializedMemberAttribute>();
+            if (attribute == null) continue;
+            Type memberType;
+            if (member is PropertyInfo property)
+            {
+                if (!property.CanRead || !property.CanWrite) continue;
+                memberType = property.PropertyType;
+            }
+            else if (member is FieldInfo field)
+            {
+                if (field.IsInitOnly) continue;
+                memberType = field.FieldType;
+            }
+            else continue;
+            members[member.Name] = new SerializedMember { Member = member, Type = memberType, Save = attribute.Save };
+        }
+        Members[type] = members;
+    }
+    public static Dictionary<string, SerializedMember> GetSerializationMembers(Type type)
+    {
+        if (!Members.ContainsKey(type)) Register(type);
+        return Members[type];
+    }
+}
+
+public interface ISerializable
+{
+
+}
+public static class Serializer
+{
+    public static void Serialize(BinaryWriter writer, ISerializable obj, bool save = true, List<string>? enabled = null)
+    {
+        Type type = obj.GetType();
+        Dictionary<string, SerializedMember> members = SerializationRegistry.GetSerializationMembers(type);
+
+        writer.Write(type.AssemblyQualifiedName ?? throw new InvalidDataException($"Type {type.Name} doesn't have a full name for some fucking reason."));
+
+        int actualCount = 0;
+        foreach (KeyValuePair<string, SerializedMember> m in members)
+        {
+            string name = m.Key;
+            SerializedMember member = m.Value;
+
+            if (enabled != null && !enabled.Contains(name))
+                continue;
+            if (save && !member.Save)
+                continue;
+
+            actualCount++;
+        }
+
+        writer.Write(actualCount);
+
+        foreach (KeyValuePair<string, SerializedMember> pair in members)
+        {
+            string name = pair.Key;
+            SerializedMember member = pair.Value;
+
+            if (enabled != null && !enabled.Contains(name))
+                continue;
+
+            if (save && !member.Save)
+                continue;
+
+            writer.Write(name);
+
+            object? value = null;
+            if (member.Member is PropertyInfo property)
+                value = property.GetValue(obj);
+            else if (member.Member is FieldInfo field)
+                value = field.GetValue(obj);
+
+            WriteValue(writer, member.Type, value, save);
+        }
+    }
+
+    public static ISerializable Deserialize(BinaryReader reader)
+    {
+        string typeName = reader.ReadString();
+        Type type = Type.GetType(typeName) ?? throw new InvalidDataException($"Type {typeName} not found.");
+        if (!type.IsAssignableTo(typeof(ISerializable))) throw new InvalidDataException($"Type {typeName} is not serializable.");
+        int memberCount = reader.ReadInt32();
+        Dictionary<string, SerializedMember> members = SerializationRegistry.GetSerializationMembers(type);
+        ISerializable obj = (ISerializable)(Activator.CreateInstance(type, nonPublic: true)
+                                            ?? throw new InvalidDataException($"Failed to create instance of type {type.FullName}."));
+        for (int i = 0; i < memberCount; i++)
+        {
+            string memberName = reader.ReadString();
+            if (!members.TryGetValue(memberName, out SerializedMember serializedMember)) throw new InvalidDataException($"Member {memberName} not found.");
+            Type memberType = serializedMember.Type;
+            object? value = ReadValue(reader, memberType);
+            if (serializedMember.Member is PropertyInfo property)
+                property.SetValue(obj, value);
+            else if (serializedMember.Member is FieldInfo field)
+                field.SetValue(obj, value);
+        }
+        return obj;
+    }
+
+    public static void Deserialize(BinaryReader reader, ISerializable obj)
+    {
+        string typeName = reader.ReadString();
+        Type type = Type.GetType(typeName) ?? throw new InvalidDataException($"Type {typeName} not found.");
+        if (type != obj.GetType()) throw new InvalidDataException($"Type {typeName} does not match object type.");
+        if (!type.IsAssignableTo(typeof(ISerializable))) throw new InvalidDataException($"Type {typeName} is not serializable.");
+        int memberCount = reader.ReadInt32();
+        Dictionary<string, SerializedMember> members = SerializationRegistry.GetSerializationMembers(type);
+        for (int i = 0; i < memberCount; i++)
+        {
+            string memberName = reader.ReadString();
+            if (!members.TryGetValue(memberName, out SerializedMember serializedMember)) throw new InvalidDataException($"Member {memberName} not found.");
+            Type memberType = serializedMember.Type;
+            object? prevValue = null;
+            {
+                if (serializedMember.Member is PropertyInfo property)
+                    prevValue = property.GetValue(obj);
+                else if (serializedMember.Member is FieldInfo field)
+                    prevValue = field.GetValue(obj);
+            }
+            object? value = ReadValueObj(reader, memberType, prevValue);
+            {
+                if (serializedMember.Member is PropertyInfo property)
+                    property.SetValue(obj, value);
+                else if (serializedMember.Member is FieldInfo field)
+                    field.SetValue(obj, value);
+            }
+        }
+    }
+    private static object ReadValueObj(BinaryReader reader, Type type, object? value)
+    {
+        if (type.IsArray)
+        {
+            if (value is Array array && array.GetType().GetElementType() == type)
+            {
+                Array.Clear(array);
+                int length = reader.ReadInt32();
+                Type elementType = type.GetElementType()!;
+                for (int i = 0; i < length; i++)
+                {
+                    object element = ReadValue(reader, elementType);
+                    array.SetValue(element, i);
+                }
+                return array;
+            }
+            else
+            {
+                int length = reader.ReadInt32();
+                Type elementType = type.GetElementType()!;
+                Array newArray = Array.CreateInstance(elementType, length);
+                for (int i = 0; i < length; i++)
+                {
+                    object element = ReadValue(reader, elementType);
+                    newArray.SetValue(element, i);
+                }
+                return newArray;
+            }
+        }
+
+        if (typeof(ISerializable).IsAssignableFrom(type))
+        {
+            if (value is ISerializable s)
+                Deserialize(reader, s);
+            else
+                return Deserialize(reader);
+            return s;
+        }
+
+        return ReadValue(reader, type);
+    }
+    private static void WriteValue(BinaryWriter writer, Type type, object? value, bool save = true)
+    {
+        if (type.IsEnum)
+        {
+            Type underlying = Enum.GetUnderlyingType(type);
+            object underlyingValue = Convert.ChangeType(value ?? 0, underlying);
+            WriteValue(writer, underlying, underlyingValue);
+            return;
+        }
+
+        if (type == typeof(int))
+        {
+            writer.Write((int)(value ?? 0));
+            return;
+        }
+        if (type == typeof(uint))
+        {
+            writer.Write((uint)(value ?? 0));
+            return;
+        }
+        if (type == typeof(float))
+        {
+            writer.Write((float)(value ?? 0f));
+            return;
+        }
+        if (type == typeof(bool))
+        {
+            writer.Write((bool)(value ?? false));
+            return;
+        }
+        if (type == typeof(string))
+        {
+            writer.Write(value as string ?? string.Empty);
+            return;
+        }
+        if (type == typeof(Vector2))
+        {
+            Vector2 v = value is Vector2 vv ? vv : default;
+            writer.Write(v.X);
+            writer.Write(v.Y);
+            return;
+        }
+        if (type == typeof(Vector3))
+        {
+            Vector3 v = value is Vector3 vv ? vv : default;
+            writer.Write(v.X);
+            writer.Write(v.Y);
+            writer.Write(v.Z);
+            return;
+        }
+        if (type == typeof(Vector4))
+        {
+            Vector4 v = value is Vector4 vv ? vv : default;
+            writer.Write(v.X);
+            writer.Write(v.Y);
+            writer.Write(v.Z);
+            writer.Write(v.W);
+            return;
+        }
+        if (type == typeof(Quaternion))
+        {
+            Quaternion v = value is Quaternion vv ? vv : default;
+            writer.Write(v.X);
+            writer.Write(v.Y);
+            writer.Write(v.Z);
+            writer.Write(v.W);
+            return;
+        }
+
+        if (type.IsArray)
+        {
+            Array? array = (Array?)value;
+            int length = array?.Length ?? 0;
+            writer.Write(length);
+
+            Type elementType = type.GetElementType()!;
+            for (int i = 0; i < length; i++)
+            {
+                object? element = array!.GetValue(i);
+                WriteValue(writer, elementType, element);
+            }
+            return;
+        }
+
+        if (typeof(ISerializable).IsAssignableFrom(type) && value != null)
+        {
+            Serialize(writer, (ISerializable)value, save);
+            return;
+        }
+
+        throw new NotSupportedException($"Cannot serialize field of type {type.FullName}.");
+    }
+
+    private static object ReadValue(BinaryReader reader, Type type)
+    {
+        if (type.IsEnum)
+        {
+            Type underlying = Enum.GetUnderlyingType(type);
+            object underlyingValue = ReadValue(reader, underlying);
+            return Enum.ToObject(type, underlyingValue);
+        }
+
+        if (type == typeof(int))
+            return reader.ReadInt32();
+        if (type == typeof(uint))
+            return reader.ReadUInt32();
+        if (type == typeof(float))
+            return reader.ReadSingle();
+        if (type == typeof(bool))
+            return reader.ReadBoolean();
+        if (type == typeof(string))
+            return reader.ReadString();
+        if (type == typeof(Vector2))
+            return new Vector2(reader.ReadSingle(), reader.ReadSingle());
+        if (type == typeof(Vector3))
+            return new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+        if (type == typeof(Vector4))
+            return new Vector4(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+        if (type == typeof(Quaternion))
+            return new Quaternion(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+        if (type.IsArray)
+        {
+            int length = reader.ReadInt32();
+            Type elementType = type.GetElementType()!;
+            Array array = Array.CreateInstance(elementType, length);
+            for (int i = 0; i < length; i++)
+            {
+                object element = ReadValue(reader, elementType);
+                array.SetValue(element, i);
+            }
+            return array;
+        }
+
+        if (typeof(ISerializable).IsAssignableFrom(type))
+        {
+            return Deserialize(reader);
+        }
+
+        throw new NotSupportedException($"Cannot deserialize field of type {type.FullName}.");
+    }
+}
